c21f3493273d96e3ca363b6f28ab917d5cee7b4c
Jason Kulak <JasonKulak@yahoo.com>
2024-04-17 22:21:31 -0500

    hopefully this makes things simpler

--- controllers/meal.js
+++ controllers/meal.js
@@ -15,28 +15,56 @@ function errorHandler(error, res){
 router.get("/seed", async (req, res) => {
     await Meal.deleteMany({})
     const meals = await Meal.create([
-        {dayOfTheWeek: "Monday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Tuesday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Wednesday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Thursday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Friday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Saturday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
-        {dayOfTheWeek: "Sunday", breakfast: "", forHowManyPeople: 1, isItPrepared: false, lunch: "", forHowManyPeople: 1, isItPrepared: false, dinner: "", forHowManyPeople: 2, isItPrepared: false},
+        {dayOfTheWeek: "Monday", meal: "Breakfast", forHowManyPeople: null, isItPrepared: true},
+        {dayOfTheWeek: "Monday", meal: "Lunch", forHowManyPeople: 10, isItPrepared: false},
+        {dayOfTheWeek: "Monday", meal: "Dinner", forHowManyPeople: 2, isItPrepared: true},
+        {dayOfTheWeek: "Tuesday", meal: "Breakfast", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Tuesday", meal: "Lunch", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Tuesday", meal: "Dinner", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Wednesday", meal: "Breakfast", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Wednesday", meal: "Lunch", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Wednesday", meal: "Dinner", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Thursday", meal: "Breakfast", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Thursday", meal: "Lunch", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Thursday", meal: "Dinner", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Friday", meal: "Breakfast", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Friday", meal: "Lunch", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Friday", meal: "Dinner", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Saturday", meal: "", forHowManyPeople: null, isItPrepared: false},
+        {dayOfTheWeek: "Sunday", meal: "", forHowManyPeople: null, isItPrepared: false},
     ])
     res.json(meals)
 })
 
 //INDUCES
 //Index Route - GET
-router.get("/", async (req, res) => {
-    try{
-        const meals = await Meal.find({})
-        res.render("meal/index.ejs", {meals})
-    }catch (err){
-        res.send(err)
-    }
-})
+//landing page option
+// router.get("/monday", async (req, res) => {
+//     try{
+//         let meals = await Meal.find({})
+//         let results = meals.filter(meal => meal.dayOfTheWeek.toLowerCase() === "monday")
+//         res.render("meal/index.ejs", {meals:results})
+//     }catch (err){
+//         res.send(err)
+//     }
+// })
+// router.get("/tuesday", async (req, res) => {
+//     try{
+//         let meals = await Meal.find({})
+//         let results = meals.filter(meal => meal.dayOfTheWeek.toLowerCase() === "tuesday")
+//         res.render("meal/index.ejs", {meals:results})
+//     }catch (err){
+//         res.send(err)
+//     }
+// })
 
+//slash route is the same as localhost:3000
+//landing page option
+router.get("/", async (req, res) => {
+    // res.render("meal/landing.ejs")
+    let meals = await Meal.find({})
+    res.render("meal/index.ejs", {meals})
+})
 //New Route - GET
 
 //Destroy Route - DELETE
@@ -49,7 +77,11 @@ router.put("/show/:id", async (req, res) => {
 })
 
 //Create Route - POST
-
+router.post("/", async (req, res) => {
+    req.body.available = Boolean(req.body.available)
+    await Meal.create(req.body)
+    res.redirect("/")
+})
 
 //Edit Route - GET
 router.get("/show/:id/edit", async (req, res) => {
--- models/meal.js
+++ models/meal.js
@@ -3,13 +3,7 @@ const mongoose = require("./connection")
 // Meal Schema
 const mealSchema = new mongoose.Schema({
     dayOfTheWeek: String,
-    breakfast: String,
-    forHowManyPeople: Number,
-    isItPrepared: Boolean,
-    lunch: String,
-    forHowManyPeople: Number,
-    isItPrepared: Boolean,
-    dinner: String,
+    meal: String,
     forHowManyPeople: Number,
     isItPrepared: Boolean,
 })
